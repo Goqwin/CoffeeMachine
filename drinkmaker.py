c70f1f8d98475d5bade4bc724a128b24b7fd20b0
godwinnmercado@hotmail.com <115321545+Goqwin@users.noreply.github.com>
2022-12-24 12:35:39 -0700

    Modifications to the drink maker class + manager
    Included a True in while loop to continually address the order issue. Had to rework the class methods for the charging + included breaks.

--- drinkmaker.py
+++ drinkmaker.py
@@ -15,7 +15,9 @@ class Drinkmaker:
         }
         Drinkmaker.nbr_of_drinks += 1
 
-    def make_drink(self, type:str, sugar, stick): # Make a drink method to take in the orders
+    def make_drink(self, type:str, sugar, stick):
+        if type not in self.protocol:
+            return "Invalid drink type"
         if sugar > 0:
             sugar = int(sugar)
             stick = "0"
@@ -23,40 +25,41 @@ class Drinkmaker:
             sugar = str("")
             stick = ""
         if type == "tea":
-                order = self.protocol[type].format(sugar=sugar, stick=stick)
-                return order
+            order = self.protocol[type].format(sugar=sugar, stick=stick)
         elif type == "coffee":
-                order = self.protocol[type].format(sugar=sugar, stick=stick)
-                return order
+            order = self.protocol[type].format(sugar=sugar, stick=stick)
         elif type == "chocolate":
-                order = self.protocol[type].format(sugar=sugar, stick=stick)
-                return order
+            order = self.protocol[type].format(sugar=sugar, stick=stick)
         else:
-            return "Invalid drink type"
+            order = "Invalid drink type"
+        return order
+
     
     def chargedrink(self, type:str):
         totalcost = 0
-        while type == "tea" or type == "coffee" or type =="chocolate":
+        while True:
             if type == "tea":
                 chargerate = 0.4
                 totalcost += chargerate
+                break
             elif type == "coffee":
                 chargerate =  0.6
                 totalcost += chargerate
+                break
             elif type == "chocolate":
                 chargerate = 0.5
                 totalcost += chargerate
+                break
         return totalcost
 
 
     def check_payment(self, totalcost, customerwallet):
         if customerwallet >= totalcost:
+            print("Your remaining balance is: ", customerwallet - totalcost)
             return True
         else:
             print("Unfortunately you do not have enough money to afford a drink. The drink. The msising amount is: ", totalcost - customerwallet)
             return False
-    
-
     
     
 
